1e141b140ab6818803095b9895cfe60396abe30d
DmytroBatyuk <dimabatyuk@gmail.com>
2020-08-07 13:09:02 +0300

    updated project

--- Application/src/main/java/com/example/android/cardemulation/CardEmulationFragment.java
+++ Application/src/main/java/com/example/android/cardemulation/CardEmulationFragment.java
@@ -17,7 +17,7 @@
 package com.example.android.cardemulation;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
